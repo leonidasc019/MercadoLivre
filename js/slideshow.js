260d80f437963300ed5060ff7574d6cc5a9df2f9
Leônidas <leonidas19992011@hotmail.com>
2023-01-30 17:21:07 -0300

    Adicionado logo, search bar e estilizado slide

--- js/slideshow.js
+++ js/slideshow.js
@@ -12,5 +12,5 @@ function nextImage(){
     if(count>5){
         count =1;
     }
-    document.getElementById("radio"+count).checked = true;
+    document.getElementById("radio"+count).checked = true
 }
